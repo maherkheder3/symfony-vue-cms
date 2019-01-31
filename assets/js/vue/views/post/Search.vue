1c8f57711f86d237469b93bf1b1bc5768aa066e8
Maher Kheder <kheder@web-netz.de>
2019-01-31 17:31:44 +0100

    search

--- assets/js/vue/views/post/Search.vue
+++ assets/js/vue/views/post/Search.vue
@@ -8,6 +8,7 @@
                     :search-input.sync="search"
                     v-model="select"
                     item-text="title"
+                    item-avatar="getAvatar"
                     item-value="id"
                     cache-items
                     class="mx-3"
@@ -15,8 +16,21 @@
                     hide-no-data
                     hide-details
                     label="What post are you from?"
-                    solo-inverted
-            ></v-autocomplete>
+                    solo-inverted >
+                <v-list-tile slot="prepend-item"
+                        class="grey--text caption">
+                    {{ items.length }} candidates found
+                </v-list-tile>
+                <!--<template slot="selection" slot-scope="{ item, selected }">-->
+                    <!--{{name}}-->
+                <!--</template>-->
+                <template slot="item" slot-scope="{item}">
+                        <v-avatar>
+                            <v-img :src="`https://unsplash.it/150/300?image=${Math.floor(Math.random() * 80) + 1}`"></v-img>
+                        </v-avatar>
+                        <span v-text="item.title" class='ml-3'></span>
+                </template>
+            </v-autocomplete>
             <!--<v-btn icon>-->
                 <!--<v-icon>more_vert</v-icon>-->
             <!--</v-btn>-->
@@ -38,6 +52,9 @@
         },
         watch: {
             search (val) {
+                if(val && val !== this.select && this.select){
+                    this.$router.push({ name:'PostDetails', params:{ postId: this.select }})
+                }
                 if(val && val !== this.select && this.querySelections(val)){
                     console.log("ok");
                 }
@@ -51,15 +68,11 @@
                 if(v.length > 0){
                     axion.get('/api/post/search/' + v).then((data) => {
                         self.states = data.data;
-                        // data.data.forEach(function (post) {
-                        //     self.states.push(post.toString())
-                        // })
                     });
                 }
 
                 setTimeout(() => {
                     this.items = this.states.filter(e => {
-                        return true;
                         return (e.title || '').toLowerCase().indexOf((v || '').toLowerCase()) > -1
                     })
                     this.loading = false
